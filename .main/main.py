e0654a1ab1d7d27c496de71041cc79ee904ab719
It'z ArU <88024858+Aru-Ofc-git@users.noreply.github.com>
2022-11-06 19:49:42 +0600

    Update main.py

--- .main/main.py
+++ .main/main.py
@@ -1,35 +1,20 @@
-#import os
+import os
 
-#if os.name == "nt":
-#	try:
-#		from bomber import windows
-#	except:
-#		os.system("pip install arubomber")
-#	try:
-#		windows.main()
-#	except:
-#		os.system("pip install --upgrade arubomber")
-#else:
-#	try:
-#		from bomber import linux
-#	except:
-#		os.system("pip install arubomber")
-#	try:
-#		linux.main()
-#	except:
-#		os.system("pip install --upgrade arubomber")
-
-
-
-import os,time
-try:
-	from bomber import bomber
-except:
-	print('arubomber not found')
-	time.sleep(5)
-	os.system('pip install --upgrade arubomber')
-
-try:
-	bomber.main()
-except:
-	print('found a error. Please connect with aru')
+if os.name == "nt":
+	try:
+		from bomber import windows
+	except:
+		os.system("pip install arubomber")
+	try:
+		windows.main()
+	except:
+		os.system("pip install --upgrade arubomber")
+else:
+	try:
+		from bomber import linux
+	except:
+		os.system("pip install arubomber")
+	try:
+		linux.main()
+	except:
+		os.system("pip install --upgrade arubomber")
